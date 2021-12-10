2d85d64981f23e633e7dd1276b4373e2ee94def7
Joseph Perez <perezj0230@gmail.com>
2021-12-09 22:54:14 -0500

    changed port and bufix

--- index.js
+++ index.js
@@ -9,84 +9,85 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 
 const {
-	handleCreateRoom,
-	handleJoinRoom,
-	handleLeaveRoom,
-	handleChangeRoomCharacteristics,
+  handleCreateRoom,
+  handleJoinRoom,
+  handleLeaveRoom,
+  handleChangeRoomCharacteristics,
 } = require("./roomsAndUsers/roomHandlers");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6021;
 
 let allRestaurants = [];
 let acceptedRestaurants = [];
 
 const getRestaurants = async () => {
-	try {
-		const yelpRes = await axios.get(process.env.YELP_RESTAURANT_ENDPOINT, {
-			headers: {
-				Authorization: `Bearer ${process.env.YELP_API_KEY}`,
-			},
-			params: {
-				location: "Waterloo, Ontario",
-			},
-		});
-		return yelpRes.data.businesses;
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const yelpRes = await axios.get(process.env.YELP_RESTAURANT_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+      },
+      params: {
+        location: "Waterloo, Ontario",
+      },
+    });
+    return yelpRes.data.businesses;
+  } catch (error) {
+    console.error(error);
+  }
 };
-allRestaurants = getRestaurants();
+
 console.log(allRestaurants);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/prototype.html");
+  res.sendFile(__dirname + "/prototype.html");
 });
 
-app.get("/restaurants", (req, res) => {
-	restaurantList = allRestaurants.map(restaurant => {
-		return restaurant.name;
-	});
-	res.json({
-		restaurants: restaurantList,
-	});
+app.get("/restaurants", async (req, res) => {
+  restaurantList = await getRestaurants();
+  restaurantList = restaurantList.map((restaurant) => {
+    return restaurant.name;
+  });
+  res.json({
+    restaurants: restaurantList,
+  });
 });
 
 app.post("/create-room", (req, res) => {
-	const { userId, roomId } = handleCreateRoom(req.body.username);
-	res.status(200).json({ userId, roomId });
+  console.log("Hi tanson");
+  const { userId, roomId } = handleCreateRoom(req.body.username);
+  res.status(200).json({ userId, roomId });
 });
 
-io.on("connection", socket => {
-	console.log(socket);
-	socket.on("accept restaurant", restaurant => {
-		console.log("accepted:", restaurant);
-		acceptedRestaurants = [...acceptedRestaurants, restaurant];
-		io.emit("new match", restaurant);
-	});
-	socket.on("chat message", msg => {
-		console.log("new msg", msg);
-		io.emit("chat message", msg);
-	});
-	console.log("a user has connected");
-	socket.on("disconnect", () => {
-		console.log("user disconnected");
-	});
+io.on("connection", (socket) => {
+  socket.on("accept restaurant", (restaurant) => {
+    console.log("accepted:", restaurant);
+    acceptedRestaurants = [...acceptedRestaurants, restaurant];
+    io.emit("new match", restaurant);
+  });
+  socket.on("chat message", (msg) => {
+    console.log("new msg", msg);
+    io.emit("chat message", msg);
+  });
+  console.log("a user has connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 
-	// user creates a new room
-	socket.on("create_room", handleCreateRoom(io, socket));
+  // user creates a new room
+  // socket.on("create_room", handleCreateRoom(io, socket));
 
-	// user joins an existing room
-	socket.on("join_room", handleJoinRoom(io));
+  // user joins an existing room
+  socket.on("join_room", handleJoinRoom(io));
 
-	// user leaves a room
-	socket.on("leave_room", handleLeaveRoom(io));
+  // user leaves a room
+  socket.on("leave_room", handleLeaveRoom(io));
 
-	// changing a room's characteristics
-	// newCharacteristics is an object with the new characteristics
-	socket.on("change_room_characteristics", handleChangeRoomCharacteristics(io));
+  // changing a room's characteristics
+  // newCharacteristics is an object with the new characteristics
+  socket.on("change_room_characteristics", handleChangeRoomCharacteristics(io));
 });
 
 // in a room, listen for "user_joined", then add this user to
@@ -94,12 +95,12 @@ io.on("connection", socket => {
 
 // if a user enters a roomID, direct that user to the room
 app.get("/:roomId", function (req, res) {
-	console.log(req.params);
-	res.sendFile(__dirname + "/prototype.html");
+  console.log(req.params);
+  res.sendFile(__dirname + "/prototype.html");
 });
 
 // if you send a text to someone, link to app store
 
 server.listen(port, () => {
-	console.log("listening on *:3000");
+  console.log("listening on *:3000");
 });
